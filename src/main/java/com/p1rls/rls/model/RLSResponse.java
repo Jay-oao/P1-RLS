2b5f0181ca21bff8d2201c0a476a92ec0fd813f4
Jayanth <jay20qaz@gmail.com>
2026-04-01 19:36:58 +0530

    e2e test

--- src/main/java/com/p1rls/rls/model/RLSResponse.java
+++ src/main/java/com/p1rls/rls/model/RLSResponse.java
@@ -1,8 +1,12 @@
 package com.p1rls.rls.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 
 @Builder
+@Data
+@AllArgsConstructor
 public class RLSResponse {
     private boolean allowed;
 
